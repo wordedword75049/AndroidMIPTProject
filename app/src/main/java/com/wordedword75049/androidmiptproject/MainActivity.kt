52744923d75f61b02b2fb9e33d14226bb01792b6
wordedword75049 <naik.iva@gmail.com>
2020-09-22 16:10:13 +0700

    Initial commit

---
+++ app/src/main/java/com/wordedword75049/androidmiptproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wordedword75049.androidmiptproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
