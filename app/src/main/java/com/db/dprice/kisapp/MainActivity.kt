c33ab84f3e560d4d96a97a7f6b2bba6c7087c551
wordedword75049 <naik.iva@gmail.com>
2020-12-16 11:15:17 +0300

    8-concurrent hw

--- app/src/main/java/com/db/dprice/kisapp/MainActivity.kt
+++ app/src/main/java/com/db/dprice/kisapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.db.dprice.kisapp
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,6 +16,7 @@ import com.db.dprice.kisapp.resview.NoteAdapter
 import com.db.dprice.kisapp.resview.NoteDetailActivity
 import com.db.dprice.kisapp.resview.NoteRepository
 import java.util.*
+import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() , NoteAdapter.Listener {
@@ -25,6 +28,8 @@ class MainActivity : AppCompatActivity() , NoteAdapter.Listener {
     private lateinit var context: Context
     private lateinit var personRepository: PersonRepository
     private lateinit var databaseHolder: DatabaseHolder
+    private val coroutineScope = MainScope()
+    private lateinit var allnotes: List<Person>
 
 
     override fun onPersonClick(id: Long) {
@@ -37,20 +42,41 @@ class MainActivity : AppCompatActivity() , NoteAdapter.Listener {
 
         context = this
         setContentView(R.layout.activity_person_list)
-        databaseHolder = DatabaseHolder(context)
+        //databaseHolder = DatabaseHolder(context)
         //textView = findViewById(R.id.textView)
-        personRepository = PersonRepository(databaseHolder)
+        //personRepository = PersonRepository(databaseHolder)
+    }
 
+    @SuppressLint("StaticFieldLeak")
+    override fun onResume() {
+        super.onResume()
 
+        coroutineScope.launch {
+            val result = withContext(Dispatchers.IO) { // Запуск корутины на фоновом поток + ожидание резульата на главном
+                databaseHolder = DatabaseHolder(context)
+                //textView = findViewById(R.id.textView)
+                personRepository = PersonRepository(databaseHolder)
+                for (i in 0..30){
+                    val person = Person()
+                    person.setName(UUID.randomUUID().toString())
+                    person.setPath(UUID.randomUUID().toString())
+                    person.setDate(Calendar.getInstance().time.toString())
 
-        for (i in 0..30){
-            val person = Person()
-            person.setName(UUID.randomUUID().toString())
-            person.setPath(UUID.randomUUID().toString())
-            person.setDate(Calendar.getInstance().time.toString())
-
-            personRepository.create(person)
+                    personRepository.create(person)
+                }
+                //LoadNotes()
+            }
+            //allnotes = result
         }
+
+        //for (i in 0..30){
+        //    val person = Person()
+        //    person.setName(UUID.randomUUID().toString())
+        //    person.setPath(UUID.randomUUID().toString())
+        //    person.setDate(Calendar.getInstance().time.toString())
+
+        //    personRepository.create(person)
+        //}
         val allnotes : List<Person> = LoadNotes()
         //textView.text = allnotes
 
@@ -58,11 +84,8 @@ class MainActivity : AppCompatActivity() , NoteAdapter.Listener {
 
         val recyclerView = findViewById<RecyclerView>(R.id.personRecyclerView)
 
-        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        } else {
-            recyclerView.layoutManager = LinearLayoutManager(this)
-        }
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         recyclerView.setHasFixedSize(true)
         recyclerView.recycledViewPool.setMaxRecycledViews(0, 5)
 
@@ -71,6 +94,11 @@ class MainActivity : AppCompatActivity() , NoteAdapter.Listener {
         adapter.setNoteList(NoteRepository.getPersonList())
         adapter.setListener(this)
 
+    }
+
+    override fun onPause() {
+        super.onPause()
+        coroutineScope.cancel()
     }
 
 }
